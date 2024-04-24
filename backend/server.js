c9e30a78bd1d5bbba2def70fe04c5a56e460b2ef
toukirislam <shovoodev@gmail.com>
2024-04-24 19:00:07 +0200

    deployment proceeds

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { configDotenv } from 'dotenv';
 import connectDB from './config/db.js';
 configDotenv();
@@ -19,6 +20,22 @@ app.use('/api/users', userRoutes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
